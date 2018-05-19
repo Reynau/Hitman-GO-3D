2956666c1a93af9047665aba55412f69cf5ecc2d
reynau <xavi.lopez.reynau@gmail.com>
2018-05-19 20:26:07 +0200

    Add Board, Node and Utility scripts

---
+++ Hitman GO 3D/Assets/Scripts/Board.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+    public static float spacing = 2f;
+
+    public static readonly Vector2[] directions =
+    {
+        new Vector2(spacing, 0f),
+        new Vector2(-spacing, 0f),
+        new Vector2(0f, spacing),
+        new Vector2(0f, -spacing)
+    };
+
+    List<Node> _allNodes = new List<Node>();
+    public List<Node> AllNodes { get { return _allNodes; } }
+
+    void Awake()
+    {
+        GetNodeList();
+    }
+
+    public void GetNodeList ()
+    {
+        Node[] nList = GameObject.FindObjectsOfType<Node>();
+        _allNodes = new List<Node>(nList);
+    }
+}
---
+++ Hitman GO 3D/Assets/Scripts/Node.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+    Vector2 _coordinate;
+    public Vector2 Coordinate { get { return Utility.Vector2Round(_coordinate); } }
+
+    List<Node> _neighborNodes = new List<Node>();
+    public List<Node> NeighborNodes { get { return _neighborNodes; } }
+
+    Board _board;
+
+    public GameObject geometry;
+    public float scaleTime = 0.3f;
+    public iTween.EaseType easeType = iTween.EaseType.easeInExpo;
+
+    public bool run = false;
+
+    public float delay = 1f;
+
+    void Awake()
+    {
+        _board = Object.FindObjectOfType<Board>();
+        _coordinate = new Vector2(transform.position.x, transform.position.z);
+    }
+
+    void Start () {
+		if (geometry != null)
+        {
+            geometry.transform.localScale = Vector3.zero;
+
+            if (run)
+            {
+                ShowGeometry();
+            }
+
+            if (_board != null)
+            {
+                _neighborNodes = FindNeighbors(_board.AllNodes);
+            }
+        }
+	}
+
+    public void ShowGeometry()
+    {
+        if (geometry != null)
+        {
+            iTween.ScaleTo(geometry, iTween.Hash(
+                "time", scaleTime,
+                "scale", Vector3.one,
+                "easetype", easeType,
+                "delay", delay
+            ));
+        }
+    }
+
+    public List<Node> FindNeighbors (List<Node> nodes)
+    {
+        List<Node> nList = new List<Node>();
+
+        foreach (Vector2 dir in Board.directions)
+        {
+            Node foundNeighbor = nodes.Find(n => n.Coordinate == _coordinate + dir);
+            nList.Add(foundNeighbor);
+        }
+
+        return nList;
+    }
+}
