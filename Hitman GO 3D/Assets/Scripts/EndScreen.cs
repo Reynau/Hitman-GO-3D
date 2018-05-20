825523f40dbaac2a24ea1e019d11132af3a49c7a
reynau <xavi.lopez.reynau@gmail.com>
2018-05-20 17:06:50 +0200

    Add EndScreen

---
+++ Hitman GO 3D/Assets/Scripts/EndScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class EndScreen : MonoBehaviour {
+    public PostProcessingProfile defaultProfile;
+    public PostProcessingProfile blurProfile;
+    public PostProcessingBehaviour cameraPostProcess;
+
+    public void EnableCameraBlur(bool enable)
+    {
+        if (defaultProfile != null && blurProfile != null && cameraPostProcess != null)
+        {
+            cameraPostProcess.profile = (enable) ? blurProfile : defaultProfile;
+        }
+    }
+}
