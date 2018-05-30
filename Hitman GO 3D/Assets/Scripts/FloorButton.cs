c11a6be18798743c75bc4672478938a77e37548a
reynau <xavi.lopez.reynau@gmail.com>
2018-05-30 14:20:34 +0200

    Add more detail to level 1 and 2

---
+++ Hitman GO 3D/Assets/Scripts/FloorButton.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorButton : MonoBehaviour {
+    GameObject _glow;
+    Material _glow_material;
+
+    Color purpleColor = new Color(0.57f, 0f, 1f);
+
+    bool _glowing = true;
+    bool _active = true;
+
+    public float changeColorDelay = 1.5f;
+
+    private void Awake()
+    {
+        _glow = gameObject.transform.Find("Glow").gameObject;
+        _glow_material = _glow.GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        StartCoroutine(ChangeColor());
+    }
+
+    IEnumerator ChangeColor()
+    {
+        for (; ; )
+        {
+            if (_active)
+            {
+                if (_glowing)
+                {
+                    iTween.ValueTo(gameObject, iTween.Hash(
+                        "from", 1f,
+                        "to", 0f,
+                        "time", changeColorDelay,
+                        "onupdate", "UpdateColor"
+                    ));
+                }
+                else
+                {
+                    iTween.ValueTo(gameObject, iTween.Hash(
+                        "from", 0f,
+                        "to", 1f,
+                        "time", changeColorDelay,
+                        "onupdate", "UpdateColor"
+                    ));
+                }
+                _glowing = !_glowing;
+            }
+            yield return new WaitForSeconds(changeColorDelay);
+        }
+    }
+
+    void UpdateColor(float val)
+    {
+        _glow_material.SetColor("_EmissionColor", new Color(val, 0f, 0f));
+    }
+
+    public void Deactivate ()
+    {
+        _active = false;
+    }
+}
---
+++ Hitman GO 3D/Assets/Scripts/TV.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TV : MonoBehaviour {
+
+    GameObject _screen;
+    Material _screen_material;
+
+    Color purpleColor = new Color(0.57f, 0f, 1f);
+    Color blueColor = new Color(0.57f, 0.57f, 1f);
+    Color newColor = new Color(1f, 0.57f, 0.57f);
+
+    public float changeColorDelay = 3f;
+
+    private void Awake()
+    {
+        _screen = gameObject.transform.Find("Screen").gameObject;
+        _screen_material = _screen.GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Start () {
+        StartCoroutine(ChangeColor());
+	}
+
+    IEnumerator ChangeColor()
+    {
+        for (;;)
+        {
+            _screen_material.SetColor("_EmissionColor", Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f));
+            yield return new WaitForSeconds(changeColorDelay);
+        }
+    }
+}
